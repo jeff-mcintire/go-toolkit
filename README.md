f84465a03213df1540d9fc930851bd7cd5cbfd98
Jeff McIntire <me@jeffmcintire.com>
2023-09-08 21:20:55 -0700

    Initial commit & adding the slugify function and tests

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# go-toolkit
-Go Toolkit
+# Web Toolkit in Go
+
+A simple example of how to create a reusable Go module with commonly used tools.
+
+The included tools are:
+
+- [ ] Read JSON
+- [ ] Write JSON
+- [ ] Produce a JSON encoded error response
+- [ ] Upload a file of files to a specified directory
+- [ ] Download a static file
+- [ ] Get a random string of length n
+- [ ] Post JSON to a remote service
+- [ ] Create a directory, including all parent directories, if it does not already exist
+- [X] Create a URL safe slug from a string
+
+## Installation
+
+`go get github.com/jeff-mcintire/go-toolkit/`
---
+++ tools.go
@@ -0,0 +1,46 @@
+package toolkit
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
+const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+
+// Tools is the type used to instantiate this module. Any variable of this type will have access
+// to all the methods with the reciever *Tools
+type Tools struct {
+	MaxFileSize        int
+	AllowedFileTypes   []string
+	MaxJSONSize        int
+	AllowUnknownFields bool
+}
+
+/*
+// RandomString returns a string of random characters of length n, using randomStringSource
+// as the source for the string
+func (t *Tools) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomStringSource)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+	return string(s)
+}
+*/
+
+// Slugify is a (very) simple means of creating a slug from a string
+func (t *Tools) Slugify(s string) (string, error) {
+	if s == "" {
+		return "", errors.New("empty string not permitted")
+	}
+
+	var re = regexp.MustCompile(`[^a-z\d]+`)
+	slug := strings.Trim(re.ReplaceAllString(strings.ToLower(s), "-"), "-")
+	if len(slug) == 0 {
+		return "", errors.New("after removing characters, slug is zero length")
+	}
+	return slug, nil
+}
